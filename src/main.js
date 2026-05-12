278b86dd3df11ea67ef56419be44cb8a13ad1c4b
MileVera <zambranomilena851@gmail.com>
2026-05-11 19:11:32 -0500

    comit inicial

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './style.css'
+import javascriptLogo from './assets/javascript.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import { setupCounter } from './counter.js'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './style.css'
+
+
